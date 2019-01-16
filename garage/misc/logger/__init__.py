58a7d0669961b7554d91f63fbcd7cb6af4f3607d
Peter Lillian <plillian@usc.edu>
2019-01-16 14:33:30 -0800

    Fixed __init__.py

--- garage/misc/logger/__init__.py
+++ garage/misc/logger/__init__.py
@@ -2,9 +2,14 @@
 This module instantiates a global logger singleton.
 """
 from garage.misc.logger.logger_inputs import TabularInput
+from garage.misc.logger.logger_outputs import CsvOutput, StdOutput, TextOutput
 from garage.misc.logger.singleton_logger import Logger
+from garage.misc.logger.tensorboard_output import TensorBoardOutput
 
 logger = Logger()
 tabular = TabularInput()
 
-__all__ = ["logger", "tabular"]
+__all__ = [
+    "tabular", "logger", "CsvOutput", "StdOutput", "TextOutput",
+    "TensorBoardOutput"
+]
